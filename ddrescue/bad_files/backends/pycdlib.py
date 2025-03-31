b1bde83be92e1ba11ed40de68b1446e83a41fccb
Matheus Afonso Martins Moreira <matheus@matheusmoreira.com>
2025-03-31 13:11:02 -0300

    backends/pycdlib: use the best path keyword
    Use the best set of extensions for reading the file system.
    The result is often cleaner paths with no abbreviations,
    leading to much more readable output.

--- ddrescue/bad_files/backends/pycdlib.py
+++ ddrescue/bad_files/backends/pycdlib.py
@@ -13,13 +13,25 @@ class PyCdlib(Backend):
     def get_inode_from(file):
         return file._ctxt.ino
 
+    def path_keyword(self):
+        if self.pycdlib.has_udf():
+            return 'udf_path'
+        elif self.pycdlib.has_joliet():
+            return 'joliet_path'
+        elif self.pycdlib.has_rock_ridge():
+            return 'rr_path'
+        else:
+            return 'iso_path'
+
     def walk(self):
-        for path, directories, files in self.pycdlib.walk(iso_path='/'):
+        path_keyword = self.path_keyword()
+
+        for path, directories, files in self.pycdlib.walk(**{path_keyword: '/'}):
             for filename in files:
 
                 full_path = path + '/' + filename
 
-                with self.pycdlib.open_file_from_iso(iso_path=full_path) as file:
+                with self.pycdlib.open_file_from_iso(**{path_keyword: full_path}) as file:
 
                     inode = PyCdlib.get_inode_from(file)
                     start = inode.extent_location() * self.pycdlib.logical_block_size
